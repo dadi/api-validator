cdc528c49ed1c11522e7ac3d4fe13c8080444a8d
Eduardo Bouças <mail@eduardoboucas.com>
2018-12-19 11:04:13 +0000

    refactor: allow null and undefined values for Boolean

--- types/boolean.js
+++ types/boolean.js
@@ -1,7 +1,7 @@
 const ValidationError = require('./../lib/validation-error')
 
 module.exports = ({schema, value}) => {
-  if (typeof value !== 'boolean') {
+  if (typeof value !== 'boolean' && value !== null && value !== undefined) {
     return new ValidationError(schema).reject()
   }
 
